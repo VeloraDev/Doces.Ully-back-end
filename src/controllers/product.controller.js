62082431a72a1d3dd9f9692237035dec4b0b9e9a
carlosdrt1 <carlosduarteinfo@gmail.com>
2025-05-11 17:57:41 -0300

    products crud

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,19 +1,91 @@
 import { ValidationError } from "sequelize";
 import ProductService from "../services/product.service.js";
 
-export default class ProductController{
-  static async create(req, res){
+export default class ProductController {
+  static async create(req, res) {
     try {
       const product = await ProductService.create(req.body);
       res.status(201).json(product);
     } catch (error) {
-        if (error instanceof ValidationError) {
+      if (error instanceof ValidationError) {
         const messages = error.errors.map((err) => err.message);
         return res.status(400).json({
           errors: messages,
         });
       }
 
+      console.log(error);
+      return res.status(500).json({
+        message: "Erro interno no servidor",
+      });
+    }
+  }
+
+  static async index(req, res) {
+    try {
+      const products = await ProductService.getProducts();
+      res.status(200).json(products);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "Erro interno no servidor",
+      });
+    }
+  }
+
+  static async show(req, res) {
+    try {
+      const product = await ProductService.getProductById(req.params.id);
+      if (!product) {
+        return res.status(404).json({
+          message: "Produto não encontrado",
+        });
+      }
+
+      return res.status(200).json(product);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "Erro interno no servidor",
+      });
+    }
+  }
+
+  static async update(req, res) {
+    try {
+      const product = await ProductService.update(req.params.id, req.body);
+      if (!product) {
+        return res.status(404).json({
+          message: "Produto não encontrado",
+        });
+      }
+
+      return res.status(200).json(product);
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        const messages = error.errors.map((err) => err.message);
+        return res.status(400).json({
+          errors: messages,
+        });
+      }
+
+      console.log(error);
+      return res.status(500).json({
+        message: "Erro interno no servidor",
+      });
+    }
+  }
+  static async delete(req, res) {
+    try {
+      const productDestroy = await ProductService.delete(req.params.id);
+      if (!productDestroy) {
+        return res.status(404).json({
+          message: "Produto não encontrado",
+        });
+      }
+
+      return res.status(204).json();
+    } catch (error) {
       console.log(error);
       return res.status(500).json({
         message: "Erro interno no servidor",
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,5 +4,9 @@ import ProductController from "../controllers/product.controller.js";
 const router = express.Router();
 
 router.post("/", ProductController.create);
+router.get("/", ProductController.index);
+router.get("/{:id}", ProductController.show);
+router.put("/{:id}", ProductController.update);
+router.delete("/{:id}", ProductController.delete);
 
 export default router;
